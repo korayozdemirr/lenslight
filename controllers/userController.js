17766de687b4c0e3de608ba6bb785dc41e285632
korayozdemirrr <koray.ozdemir11@gmail.com>
2022-10-16 11:35:56 +0300

    ders 11 bitti

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,50 +1,58 @@
 import User from "../models/userModel.js";
-import bcrypt from "bcrypt"
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 const createUser = async (req, res) => {
   try {
     const user = await User.create(req.body);
     res.status(201).json({
       succeded: true,
-      user
+      user,
     });
   } catch (error) {
     res.status(500).json({
       succeded: false,
-      error
+      error,
     });
   }
 };
 
 const loginUser = async (req, res) => {
   try {
-    const {username, password} = req.body;
-    const user = await User.findOne({username});
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
 
     let same = false;
-    if(user){
-      same = await bcrypt.compare(password, user.password)
-    }else{
+    if (user) {
+      same = await bcrypt.compare(password, user.password);
+    } else {
       return res.status(401).json({
         succeded: false,
-        error:"There is no such user.."
+        error: "There is no such user..",
       });
     }
-    if(same){
-      res.status(200).send("You are logged in");
-    }else{
+    if (same) {
+      res.status(200).json({
+        user,
+        token:createToken(user._id)
+      })
+    } else {
       res.status(401).json({
         succeded: false,
-        error:"Passwords are not matched"
+        error: "Passwords are not matched",
       });
     }
-
   } catch (error) {
     res.status(500).json({
       succeded: false,
-      error
+      error,
     });
   }
 };
 
+const createToken = (userId) => {
+  return jwt.sign({ userId }, process.env.JWT_SECRET, {
+    expiresIn: "1d",
+  });
+};
 
 export { createUser, loginUser };
